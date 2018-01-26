f2e1111e3ea58b43c4e13cda658ed11194e197d9
alyssamhope <berkeleycole@users.noreply.github.com>
2018-01-26 13:30:43 -0800

    Update Week1-example-problems.js

--- Week1-example-problems.js
+++ Week1-example-problems.js
@@ -2,9 +2,9 @@
 
 // 2. Write about yourself using an object. Include at least three properties of you and store your object in a variable with your name.
 
-// 3. Create an array with at least 4 items inside it. Show how to access two values from the array in two different ways.
+// 3. Create an array with at least 4 items inside it. Then, access one of the values and console log it. 
 
-// 4. Have the function AlphabetSoup(str) take the str string parameter being passed and return a string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string. 
+// 4. Have the function AlphabetSoup(str) take an str string parameter and return a string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string. This will require some googling.
 
 // Input:"hooplah"
 // Output:"ahhloop"
@@ -23,6 +23,14 @@ AlphabetSoup(readline());
 
 var nums = [1, 5, 88, 2, 5, 42, 57, 101]
 
-var nouns = ["ducks", "telephone booth", "the enterprise", "robots", "amazon", "eraser", "zafod", "a"]
+var nouns = ["duck", "telephone booths", "the enterprise", "robots", "amazon", "eraser", "zafod", "a"]
 
-// output of the first function should be: "1 ducks"
+// output of the first time through the loop should be: "1 ducks"
+
+// 6. Create an object "coffeeMachine" with attributes coffee and water. These attributes should have boolean values - false meaning there is not enough, and true meaning there is enough. Next, create a method for our coffeeMachine object to run the machine. The result of this method should be a message, saying that coffee is ready. There must be enough water and coffee in order to run the machine. If there aren't enough of either, the coffe maker should send you a message saying "add more ____."
+
+var coffeeMachine = {
+
+// your code here
+
+}
